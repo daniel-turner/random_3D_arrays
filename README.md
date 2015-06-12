2cc64fc80e0011f577ef334f9c074ae6613cd8ad
danturne <danturne@yahoo.com>
2015-06-12 10:03:59 -1000

    completed exercise

---
+++ README.md
@@ -0,0 +1,18 @@
+##Your Challenge
+
+Write an function that takes 2 inputs:
+
+1. Number of rows
+2. Number of columns
+
+Your function should do the following:
+
+1. Generate a multi-dimensional array with the specified number of rows and columns in the array populated with random numbers between 1 and 100.
+2. Sorts the array with natural numeric sorting.
+3. Returns the sorted array
+
+Ex.
+`generator(3, 3)`
+
+Output:
+[[1, 2 ,3], [4,5,6], [7,8,9]]
---
+++ sort_3D_array.js
@@ -0,0 +1,62 @@
+function sort_3D_array(rows,columns) {
+
+  //generate random array
+  var array = [];
+  var newRow;
+
+  for(var c = 0;c<columns;c++) {
+
+    newRow = [];
+
+    for(var r = 0;r<rows;r++) {
+
+      newRow.push(Math.floor(Math.random()*99 + 1));
+    }
+
+    array.push(newRow);
+  }
+
+  //sort 2d
+
+  var newArray = [];
+
+  for(var i = 0;i<array.length;i++) {
+
+    for(var j = 0;j<array[i].length;j++) {
+
+      newArray.push(array[i][j]);
+    }
+  }
+
+  newArray = newArray.sort(function(x,y) {
+
+    return x-y;
+  });
+
+
+
+  //sort 3d
+
+  var out = [];
+
+  while(newArray.length > 0) {
+
+    for(var i = 0;i<columns;i++) {
+
+      newRow = [];
+
+      for(var j = 0;j<rows;j++) {
+
+        newRow.push(newArray.splice(0,1));
+      }
+
+      out.push(newRow);
+    }
+  }
+
+  console.log(out);
+
+  return out;
+}
+
+sort_3D_array(7,8);
